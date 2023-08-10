ad4973ee078c794a3ff47b662dff9f4330d56c6e
BacelarIvanildo <ivanildo.mendes@gmail.com>
2023-08-10 17:07:32 -0300

    Correção da criação da tabela.

--- wca.compras.backend/wca.compras.data/Migrations/20230807002529_CreateTable_UsuarioReembolsoComplemento.cs
+++ wca.compras.backend/wca.compras.data/Migrations/20230807002529_CreateTable_UsuarioReembolsoComplemento.cs
@@ -13,23 +13,17 @@ namespace wca.compras.data.Migrations
                 columns: table => new
                 {
                     usuario_id = table.Column<int>(type: "int", nullable: false),
-                    gestor_id = table.Column<int>(type: "int", nullable: false),
+                    gestor_id = table.Column<int>(type: "int", nullable: true),
                     cargo = table.Column<string>(type: "varchar(100)", nullable: true)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_UsuarioReembolsoComplemento", x => x.usuario_id);
-                    table.ForeignKey(
-                        name: "FK_UsuarioReembolsoComplemento_Usuarios_gestor_id",
-                        column: x => x.gestor_id,
-                        principalTable: "Usuarios",
-                        principalColumn: "id");
-                    table.ForeignKey(
-                        name: "FK_UsuarioReembolsoComplemento_Usuarios_usuario_id",
-                        column: x => x.usuario_id,
-                        principalTable: "Usuarios",
-                        principalColumn: "id");
                 });
+            migrationBuilder.CreateIndex(
+                        name: "IX_UsuarioReembolsoComplemento_usuario_id",
+                        table: "UsuarioReembolsoComplemento",
+                        column: "usuario_id");
 
             migrationBuilder.CreateIndex(
                 name: "IX_UsuarioReembolsoComplemento_gestor_id",
