166b04bdc5a7a43cbdf82a5ad42f6ca864df696f
BacelarIvanildo <ivanildo.mendes@gmail.com>
2023-09-29 01:00:12 -0300

    Versão 1.4.0
    (cherry picked from commit db94427d79b87e7e36d301b1e300acdacc70e229)

--- wcacompras-frontend/vue.config.js
+++ wcacompras-frontend/vue.config.js
@@ -12,8 +12,8 @@ module.exports = defineConfig({
         logLevel: 'error'
       },
       '^/reembolsoapi': {
-        target: 'https://wca-api-reembolso-hml.azurewebsites.net/api',
-        //target: 'http://localhost:5100/api',
+        //target: 'https://wca-api-reembolso-hml.azurewebsites.net/api',
+        target: 'http://localhost:5100/api',
         pathRewrite: {'^/reembolsoapi': ''},
         changeOrigin: true,
         secure: false,
