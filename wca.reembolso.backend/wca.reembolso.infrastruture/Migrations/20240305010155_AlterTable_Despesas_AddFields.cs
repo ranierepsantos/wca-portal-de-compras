002baeac9e809037ca17413935f1af33b18f5ca2
BacelarIvanildo <ivanildo.mendes@gmail.com>
2024-03-05 20:54:35 -0300

    [REEMBOLSO][FEATURE] - adicionar observação - adicionar botão para conferido - poder reprovar despesa Ainda em andamento
    (cherry picked from commit 2834d351ca3928f726157b5a7f43e7c6115bf600)
    (cherry picked from commit 142b73b165e102f262f25c95608b74d9ee43801e)

---
+++ wca.reembolso.backend/wca.reembolso.infrastruture/Migrations/20240305010155_AlterTable_Despesas_AddFields.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wca.reembolso.infrastruture.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterTable_Despesas_AddFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "aprovada",
+                table: "Despesas",
+                type: "tinyint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "conferida",
+                table: "Despesas",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "observacao",
+                table: "Despesas",
+                type: "varchar(500)",
+                nullable: true);
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "aprovada",
+                table: "Despesas");
+
+            migrationBuilder.DropColumn(
+                name: "conferida",
+                table: "Despesas");
+
+            migrationBuilder.DropColumn(
+                name: "observacao",
+                table: "Despesas");
+        }
+    }
+}
