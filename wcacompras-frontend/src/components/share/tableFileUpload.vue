c61a1d931ec6422039c0979addc18d0973a15f15
BacelarIvanildo <ivanildo.mendes@gmail.com>
2024-03-21 18:02:14 -0300

    [SHARE] - Alterar anexo para definir o tipo (Ficha EPI, Apontamento, etc), quando anexar determinado tipo, marcar o combo do item como concluído.
    (cherry picked from commit 64a826d229ed8e2a57925d2f97f667969c77e33c)

--- wcacompras-frontend/src/components/share/tableFileUpload.vue
+++ wcacompras-frontend/src/components/share/tableFileUpload.vue
@@ -1,58 +1,79 @@
 <template>
   <div>
     <v-row>
-            <v-col>
-                <h3 class="text-left text-grey mt-2">ANEXOS</h3>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <v-file-input
-      accept=".pdf,.doc,.docx"
-      label="Anexar arquivo"
-      variant="outlined"
-      density="compact"
-      @change="handleFile()"
-      :clearable="true" 
-      v-model="fileInput"
-      v-show="!isReadOnly"
-    ></v-file-input>
-    <v-table>
-      <thead>
-        <tr>
-          <th class="text-left text-grey">Arquivo</th>
-          <th class="text-center text-grey">Ações</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in anexos" :key="item.id">
-          <td class="text-left">
-            <v-icon icon="mdi-file-document-check-outline"></v-icon>
-            &nbsp;{{ item.descricao }}
-          </td>
-          <td class="text-center">
-            <v-btn
-              icon="mdi-file-download-outline"
-              variant="plain"
+      <v-col>
+        <h3 class="text-left text-grey mt-2">ANEXOS</h3>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-row>
+          <v-col cols="3">
+            <v-select
+              label="Tipo"
+              :items="comboItems"
+              density="compact"
+              item-title="text"
+              item-value="text"
+              variant="outlined"
               color="primary"
-              @click="downloadFile(item.caminhoArquivo, item.descricao)"
-            ></v-btn>
-            <v-btn
-              variant="plain"
-              color="error"
-              icon="mdi-trash-can-outline"
-              title="Exluir"
-              @click="removeFile(item)"
+              v-model="tipo"
               v-show="!isReadOnly"
-            >
-            </v-btn>
-          </td>
-        </tr>
-      </tbody>
-    </v-table>
-  </v-col>
-          </v-row>
-          
+            ></v-select>
+          </v-col>
+          <v-col>
+            <v-file-input
+              accept=".pdf,.doc,.docx"
+              label="Anexar arquivo"
+              variant="outlined"
+              density="compact"
+              @change="handleFile()"
+              :clearable="true"
+              v-model="fileInput"
+              v-show="!isReadOnly"
+              :disabled="tipo == null"
+            ></v-file-input>
+          </v-col>
+        </v-row>
+        
+
+        <v-table>
+          <thead>
+            <tr>
+              <th class="text-left text-grey">Arquivo</th>
+              <th class="text-left text-grey">Tipo</th>
+              <th class="text-center text-grey">Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in anexos" :key="item.id">
+              <td class="text-left">
+                <v-icon icon="mdi-file-document-check-outline"></v-icon>
+                &nbsp;{{ item.descricao }}
+              </td>
+              <td class="text-left">{{ item.tipo }}</td>
+              <td class="text-center">
+                <v-btn
+                  icon="mdi-file-download-outline"
+                  variant="plain"
+                  color="primary"
+                  @click="downloadFile(item.caminhoArquivo, item.descricao)"
+                ></v-btn>
+                <v-btn
+                  variant="plain"
+                  color="error"
+                  icon="mdi-trash-can-outline"
+                  title="Exluir"
+                  @click="removeFile(item)"
+                  v-show="!isReadOnly"
+                >
+                </v-btn>
+              </td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -69,11 +90,19 @@ const props = defineProps({
       return [];
     },
   },
-  isReadOnly: {type: Boolean, default: false},
+  comboItems: {
+    type: Array,
+    default: function () {
+      return [];
+    },
+  },
+  isReadOnly: { type: Boolean, default: false },
 });
 
 const swal = inject("$swal");
 const fileInput = ref([]);
+const tipo = ref(null);
+const emit = defineEmits(['changeStatus'])
 
 async function handleFile() {
   if (fileInput.value.length > 0) {
@@ -82,7 +111,7 @@ async function handleFile() {
     let anexo = new Anexo();
     anexo.caminhoArquivo = arquivo;
     anexo.descricao = file.name;
-
+    anexo.tipo = tipo.value 
     let _check = props.anexos.find(
       (q) => q.descricao.toLowerCase() == anexo.descricao.toLowerCase()
     );
@@ -97,8 +126,10 @@ async function handleFile() {
         showConfirmButton: false,
         timer: 3000,
       });
-    } else props.anexos.push(anexo);
-
+    } else {
+      props.anexos.push(anexo);
+      emit('changeStatus', anexo.tipo);
+    }
     _check = null;
     fileInput.value = [];
   }
@@ -135,8 +166,7 @@ async function removeFile(item) {
     let index = props.anexos.findIndex((q) => q.descricao == item.descricao);
     if (index > -1) {
       props.anexos.splice(index, 1);
-    }  
+    }
   }
-  
 }
 </script>
