b9214ba03c7160a9a439d8c5a35e7735d1cd1d2b
BacelarIvanildo <ivanildo.mendes@gmail.com>
2024-03-23 19:13:36 -0300

    [REEMBOLSO][BUGFIX] - quando usuário não tem conta corrente, ocorre erro ao registrar pagamento
    (cherry picked from commit a693df128b6ce58ff77f5c7fbd2f4b483ca73ba8)

--- wca.reembolso.backend/wca.reembolso.application/Features/Conta/Queries/ContaCorrenteByUsuarioQuerieHandler.cs
+++ wca.reembolso.backend/wca.reembolso.application/Features/Conta/Queries/ContaCorrenteByUsuarioQuerieHandler.cs
@@ -4,6 +4,7 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using wca.reembolso.application.Contracts.Persistence;
+using wca.reembolso.application.Features.Conta.Commands;
 using wca.reembolso.application.Features.Conta.Common;
 using wca.reembolso.domain.Entities;
 
@@ -15,12 +16,14 @@ namespace wca.reembolso.application.Features.Conta.Queries
         private readonly IRepositoryManager _repository;
         private readonly IMapper _mapper;
         private readonly ILogger<ContaCorrenteByUsuarioQuerieHandler> _logger;
+        private readonly IMediator _mediator;
 
-        public ContaCorrenteByUsuarioQuerieHandler(IRepositoryManager repository, IMapper mapper, ILogger<ContaCorrenteByUsuarioQuerieHandler> logger)
+        public ContaCorrenteByUsuarioQuerieHandler(IRepositoryManager repository, IMapper mapper, ILogger<ContaCorrenteByUsuarioQuerieHandler> logger, IMediator mediator)
         {
             _repository = repository;
             _mapper = mapper;
             _logger = logger;
+            _mediator = mediator;
         }
 
         public async Task<ErrorOr<ContaCorrenteResponse>> Handle(ContaCorrenteByUsuarioQuery request, CancellationToken cancellationToken)
@@ -34,8 +37,12 @@ namespace wca.reembolso.application.Features.Conta.Queries
             ContaCorrente? conta = await query.FirstOrDefaultAsync(cancellationToken: cancellationToken);
             
             if (conta is null) {
-                _logger.LogInformation("Conta do usuario {0}, não localizado", request.UsuarioId);
-                return Error.NotFound("Conta não localizada");
+                _logger.LogInformation("Conta do usuario {0}, não localizado - criando nova conta", request.UsuarioId);
+                ContaCorrenteCreateUpdateCommand command = new(
+                    UsuarioId: request.UsuarioId,
+                    Transacoes: new List<Transacao>()
+                );
+                return await _mediator.Send(command, cancellationToken);
             }
 
             return _mapper.Map<ContaCorrenteResponse>(conta);
