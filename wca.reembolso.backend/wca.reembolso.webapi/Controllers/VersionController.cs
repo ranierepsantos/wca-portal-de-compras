5a7b9b4d5b26913f6f02a8b8313337312b755593
BacelarIvanildo <ivanildo.mendes@gmail.com>
2024-03-28 12:10:51 -0300

    [REEMBOLSO] - Versão da api

---
+++ wca.reembolso.backend/wca.reembolso.webapi/Controllers/VersionController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace wca.reembolso.webapi.Controllers
+{
+    public class VersionController : ApiController
+    {
+        [HttpGet("GetVersion")]
+        public IActionResult GetVersion()
+        {
+            return Ok("1.0.15");
+        }
+    }
+}
