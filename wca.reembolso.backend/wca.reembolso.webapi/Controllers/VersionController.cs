30b8f23ade9b7829c9d4c4a56a2220fbb927e162
BacelarIvanildo <ivanildo.mendes@gmail.com>
2024-04-08 22:54:57 -0300

    Versão 1.5.1

--- wca.reembolso.backend/wca.reembolso.webapi/Controllers/VersionController.cs
+++ wca.reembolso.backend/wca.reembolso.webapi/Controllers/VersionController.cs
@@ -7,7 +7,7 @@ namespace wca.reembolso.webapi.Controllers
         [HttpGet("GetVersion")]
         public IActionResult GetVersion()
         {
-            return Ok("1.0.15");
+            return Ok("1.0.16");
         }
     }
 }
