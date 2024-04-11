456dc666117741b7bb96442d19e43f9b0e246cbe
BacelarIvanildo <ivanildo.mendes@gmail.com>
2024-04-11 11:20:21 -0300

    [REEMBOLSO] - Implementado azure function para checar solicitações vencidas
    (cherry picked from commit 756a9b36c76496a4422999d5a2de56e754c4485f)

---
+++ wca.compras.backend/wca.compras.functions/ReembolsoChecarSolicitacaoVencida.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Net.Http.Headers;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using TimerTriggerAttribute = Microsoft.Azure.Functions.Worker.TimerTriggerAttribute;
+
+namespace wca.compras.functions
+{
+    public class ReembolsoChecarSolicitacaoVencida
+    {
+        private readonly ILogger _logger;
+
+        public ReembolsoChecarSolicitacaoVencida(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<ReembolsoChecarSolicitacaoVencida>();
+        }
+
+        [Function("ReembolsoChecarSolicitacaoVencida")]
+        public async Task Run([TimerTrigger("0 24 4 * * *")] MyInfo myTimer)
+        {
+            _logger.LogInformation($"Timer trigger function executed at: {DateTime.Now}");
+
+            HttpClient client = new();
+
+            //string baseUrl = "https://wca-backend.azurewebsites.net/"; //produ��o
+            string baseUrl = "https://wca-api-reembolso-hml.azurewebsites.net/"; //hml
+            //string baseUrl = "https://localhost:7235/"; //desenvolvimento
+
+            client.BaseAddress = new Uri(baseUrl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            var path = "api/Solicitacao/ChecarVencidos";
+
+            HttpResponseMessage response = await client.GetAsync(path);
+
+            if (response.IsSuccessStatusCode)
+            {
+                _logger.LogInformation($"{DateTime.Now} ExecuteSchedule success!");
+            }
+            else
+            {
+                _logger.LogInformation($"{DateTime.Now} ExecuteSchedule problem!");
+            }
+
+            if (myTimer.ScheduleStatus is not null)
+            {
+                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            }
+        }
+    }
+
+    
+}
