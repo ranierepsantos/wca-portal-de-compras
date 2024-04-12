fa735c8cd0bba7d8bc1bd9ef6ceda60dd9a4a2b9
BacelarIvanildo <ivanildo.mendes@gmail.com>
2024-04-12 19:35:55 -0300

    [REEMBOLSO] - Integração chatbot

---
+++ wca.reembolso.backend/wca.reembolso.application/Contracts/NorgeChatBot/IIntegrationNorgeChatBot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wca.reembolso.application.Contracts.NorgeChatBot
+{
+    public interface IIntegrationNorgeChatBot
+    {
+        Task<Response> Send(int number, string message);
+    }
+}
---
+++ wca.reembolso.backend/wca.reembolso.application/Contracts/NorgeChatBot/Models.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace wca.reembolso.application.Contracts.NorgeChatBot
+{
+    public sealed class Message
+    {
+        public int Number { get; set; }
+        public string Body { get; set; }
+    }
+
+    public sealed class Response
+    {
+        [JsonPropertyName("mensagem")]
+        public string Mensagem { get; set; }
+
+        [JsonPropertyName("error")]
+        public string Error { get; set; }
+    }
+
+}
