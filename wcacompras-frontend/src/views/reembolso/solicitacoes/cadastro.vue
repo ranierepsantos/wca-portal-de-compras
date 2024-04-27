3ad9439d89a7bbf4040c2cc487c84519bfb3ce71
BacelarIvanildo <ivanildo.mendes@gmail.com>
2024-04-26 21:49:06 -0300

    [REEMBOLSO][BUGFIX] - Correção no envio de centro de custo null, quando modo edição
    (cherry picked from commit 80a2b565e99f72a20dcb6e60ca735ceb4fde98e6)

--- wcacompras-frontend/src/views/reembolso/solicitacoes/cadastro.vue
+++ wcacompras-frontend/src/views/reembolso/solicitacoes/cadastro.vue
@@ -539,8 +539,8 @@ watch(() => solicitacao.value.clienteId, async (newValue,oldValue) => {
     let cliente = clientes.value.find(q => q.id == newValue)
     if (cliente) {
       listCentroCusto.value = await useUsuarioStore().getCentrosdeCusto(authStore.user.id, cliente.id)
-    if (!isColaborador.value)
-      solicitacao.value.centroCustoId = null
+      if (!isColaborador.value && solicitacao.value.id == 0)
+        solicitacao.value.centroCustoId = null
     }
   }
   
@@ -738,9 +738,17 @@ async function salvar() {
       };
 
       let response = await swal.fire(options);
-      if (response.isConfirmed) {
+      if (response.isConfirmed) 
         data.status = 5; //5 - aguardando conferência
-      }
+        
+    } else if (data.tipoSolicitacao == 2 && (data.status == 3 || data.status == 12) && data.despesa.length == 0) {
+      let options = {
+        title: "Informação",
+        html: "Não é possível enviar para aprovação sem cadastro de despesas!",
+        icon: "question",
+      };
+      await swal.fire(options);
+      return
     }
     
     //trocar os id despesa negativos por 0
